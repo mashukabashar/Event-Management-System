5c2d923e771c13243cf5e4e5697d4c48adee0490
mashukabashar <diyamoni422@gmail.com>
2026-04-08 17:20:24 +0600

    ready for render deployment

--- users/signals.py
+++ users/signals.py
@@ -8,21 +8,21 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-@receiver(post_save, sender=User)
-def send_activation_email(sender, instance, created, **kwargs):
-    if created:
-        token = default_token_generator.make_token(instance)
-        activation_url = f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
+# @receiver(post_save, sender=User)
+# def send_activation_email(sender, instance, created, **kwargs):
+#     if created:
+#         token = default_token_generator.make_token(instance)
+#         activation_url = f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
 
-        subject = 'Activate Your Account'
-        message = f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}\n\nThank You!'
-        recipient_list = [instance.email]
+#         subject = 'Activate Your Account'
+#         message = f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}\n\nThank You!'
+#         recipient_list = [instance.email]
 
-        try:
-            send_mail(subject, message,
-                    settings.EMAIL_HOST_USER, recipient_list, fail_silently=False)
-        except Exception as e:
-            logger.error(f"Activation email failed for {instance.email}: {str(e)}")
+#         try:
+#             send_mail(subject, message,
+#                     settings.EMAIL_HOST_USER, recipient_list, fail_silently=False)
+#         except Exception as e:
+#             logger.error(f"Activation email failed for {instance.email}: {str(e)}")
 
 
 @receiver(post_save, sender=User)
