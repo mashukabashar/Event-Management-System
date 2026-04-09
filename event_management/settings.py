9c6c2ab1e4eccce4c707be0492ca4d8d7809c0cc
mashukabashar <diyamoni422@gmail.com>
2026-04-09 12:35:29 +0600

    FINAL-MASHUKA

--- event_management/settings.py
+++ event_management/settings.py
@@ -158,14 +158,15 @@ MEDIA_ROOT=BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="diyamoni422@gmail.com"
 EMAIL_HOST_PASSWORD="elbh qvcq kuip rmfu"
 
-FRONTEND_URL='http://127.0.0.1:8000'
+FRONTEND_URL='https://*.onrender.com'
 LOGIN_URL='sign-in'
 
 
