b843868ad00cadc8efe8427a67e5f1d6f1674c72
mashukabashar <diyamoni422@gmail.com>
2026-04-09 12:43:43 +0600

    ssl-add

--- event_management/settings.py
+++ event_management/settings.py
@@ -161,8 +161,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_USE_TLS=True
-EMAIL_PORT=587
+EMAIL_USE_TLS=False
+EMAIL_USE_SSL=True
+EMAIL_PORT=465
 EMAIL_HOST_USER="diyamoni422@gmail.com"
 EMAIL_HOST_PASSWORD="elbh qvcq kuip rmfu"
 
